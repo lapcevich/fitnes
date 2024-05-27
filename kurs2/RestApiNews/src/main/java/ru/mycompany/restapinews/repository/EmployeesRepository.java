3763edf1fbb7379505f7f9b4ce21d2260f56f80f
lapcevich <163328248+lapcevich@users.noreply.github.com>
2024-05-27 17:59:01 +0300

    Delete kurs2/RestApiNews directory

--- kurs2/RestApiNews/src/main/java/ru/mycompany/restapinews/repository/EmployeesRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.mycompany.restapinews.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.mycompany.restapinews.model.Employees;
-
-public interface EmployeesRepository extends JpaRepository<Employees, Long> {
-}
--- kurs2/RestApiNews/src/main/java/ru/mycompany/restapinews/repository/HallsRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.mycompany.restapinews.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.mycompany.restapinews.model.Halls;
-
-public interface HallsRepository extends JpaRepository<Halls, Long> {
-}
